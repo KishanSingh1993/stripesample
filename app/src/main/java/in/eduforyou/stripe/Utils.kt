ae73071074c67df841682a1821c205afcc3bd17c
shivam.gupta9 <kishang.90@gmail.com>
2024-05-16 12:57:55 +0530

    new changes

--- app/src/main/java/in/eduforyou/stripe/Utils.kt
+++ app/src/main/java/in/eduforyou/stripe/Utils.kt
@@ -2,7 +2,7 @@ package `in`.eduforyou.stripe
 
 object Utils {
 
-    const val PUBLISHABLE_KEY = "pk_test_51PGwWuSGwOFmuX53qU9OTSDF8MIGnGkNXmflVR3ptGFxyHrOC3UVYjkm8o7HOEffyEniMKQiuXTC4Sx0ObuBxtqZ00ieeYrXAz"
+    const val PUBLISHABLE_KEY = ""
 
-    const val SECRET_KEY = "sk_test_51PGwWuSGwOFmuX53xObWX2LuJ2YWfuWawSN1FyCdWvJexPmHUAqWxD2iuG6zVASyepYQ17vsKGDcPFTmHm4amdH30047c6eDlB"
+    const val SECRET_KEY = ""
 }
